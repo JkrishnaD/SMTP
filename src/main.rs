66dd470c2eff753090eda6324181c9467fe5c9fd
JKrishnaD <jayak5063@gmail.com>
2026-03-06 11:18:27 +0530

    feat: add tcp listener with response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use tokio::{io::AsyncWriteExt, net::TcpListener};
+
 #[tokio::main]
-async fn main() {
-    println!("Starting SMTP server...");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listerner = TcpListener::bind("127.0.0.1:2525").await?;
+    println!("Starting SMTP server on 2525");
+
+    loop {
+        let (mut socket, addr) = listerner.accept().await?;
+        println!("Accepted connection from: {}", addr);
+        println!("Socket stream: {:?}", socket);
+
+        socket.write_all(b"220 simple-smtp ready\n").await?;
+    }
 }
